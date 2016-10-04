25f6aabd342bc2d612468e7650cd9b7c6c595b8c
DevlinZA <jaco.vanzyl@creative-sound.net>
2016-10-04 20:30:26 +0200

    Recording is in ( met ng n paar minor bugs )

--- PschyHealth/Forms/frmRecord.Designer.cs
+++ PschyHealth/Forms/frmRecord.Designer.cs
@@ -28,13 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.radialMenu1 = new DevComponents.DotNetBar.RadialMenu();
-            this.rdRecord = new DevComponents.DotNetBar.RadialMenuItem();
-            this.rdSave = new DevComponents.DotNetBar.RadialMenuItem();
-            this.rdPause = new DevComponents.DotNetBar.RadialMenuItem();
+            this.components = new System.ComponentModel.Container();
+            this.txtName = new MetroFramework.Controls.MetroTextBox();
             this.rdPlay = new DevComponents.DotNetBar.RadialMenuItem();
-            this.metroTextBox1 = new MetroFramework.Controls.MetroTextBox();
+            this.rdPause = new DevComponents.DotNetBar.RadialMenuItem();
+            this.rdSave = new DevComponents.DotNetBar.RadialMenuItem();
+            this.rdRecord = new DevComponents.DotNetBar.RadialMenuItem();
+            this.radialMenu1 = new DevComponents.DotNetBar.RadialMenu();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblPlayBack = new MetroFramework.Controls.MetroLabel();
+            this.lblTime = new MetroFramework.Controls.MetroLabel();
+            this.pbWave = new System.Windows.Forms.PictureBox();
+            this.pbRec = new System.Windows.Forms.PictureBox();
+            this.lblPlayName = new MetroFramework.Controls.MetroLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pbWave)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbRec)).BeginInit();
             this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            // 
+            // 
+            // 
+            this.txtName.CustomButton.Image = null;
+            this.txtName.CustomButton.Location = new System.Drawing.Point(53, 1);
+            this.txtName.CustomButton.Name = "";
+            this.txtName.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.txtName.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.txtName.CustomButton.TabIndex = 1;
+            this.txtName.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.txtName.CustomButton.UseSelectable = true;
+            this.txtName.CustomButton.Visible = false;
+            this.txtName.Lines = new string[0];
+            this.txtName.Location = new System.Drawing.Point(212, 39);
+            this.txtName.MaxLength = 32767;
+            this.txtName.Name = "txtName";
+            this.txtName.PasswordChar = '\0';
+            this.txtName.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.txtName.SelectedText = "";
+            this.txtName.SelectionLength = 0;
+            this.txtName.SelectionStart = 0;
+            this.txtName.Size = new System.Drawing.Size(75, 23);
+            this.txtName.TabIndex = 1;
+            this.txtName.UseSelectable = true;
+            this.txtName.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.txtName.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // rdPlay
+            // 
+            this.rdPlay.Name = "rdPlay";
+            this.rdPlay.Symbol = "";
+            this.rdPlay.Text = "Play";
+            // 
+            // rdPause
+            // 
+            this.rdPause.Name = "rdPause";
+            this.rdPause.Symbol = "";
+            this.rdPause.Text = "Pause";
+            // 
+            // rdSave
+            // 
+            this.rdSave.Name = "rdSave";
+            this.rdSave.Symbol = "";
+            this.rdSave.Text = "Save/Stop";
+            // 
+            // rdRecord
+            // 
+            this.rdRecord.Name = "rdRecord";
+            this.rdRecord.Symbol = "";
+            this.rdRecord.Text = "Record";
             // 
             // radialMenu1
             // 
@@ -51,82 +113,91 @@
             this.radialMenu1.TabIndex = 0;
             this.radialMenu1.Text = "radialMenu1";
             // 
-            // rdRecord
+            // lblPlayBack
             // 
-            this.rdRecord.Name = "rdRecord";
-            this.rdRecord.Symbol = "";
-            this.rdRecord.Text = "Record";
+            this.lblPlayBack.AutoSize = true;
+            this.lblPlayBack.Location = new System.Drawing.Point(184, 127);
+            this.lblPlayBack.Name = "lblPlayBack";
+            this.lblPlayBack.Size = new System.Drawing.Size(136, 20);
+            this.lblPlayBack.TabIndex = 56;
+            this.lblPlayBack.Text = "Playback in progress";
+            this.lblPlayBack.UseStyleColors = true;
+            this.lblPlayBack.Visible = false;
             // 
-            // rdSave
+            // lblTime
             // 
-            this.rdSave.Name = "rdSave";
-            this.rdSave.Symbol = "";
-            this.rdSave.Text = "Save/Stop";
+            this.lblTime.AutoSize = true;
+            this.lblTime.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.lblTime.Location = new System.Drawing.Point(216, 93);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(0, 0);
+            this.lblTime.TabIndex = 53;
             // 
-            // rdPause
+            // pbWave
             // 
-            this.rdPause.Name = "rdPause";
-            this.rdPause.Symbol = "";
-            this.rdPause.Text = "Pause";
+            this.pbWave.Image = global::PschyHealth.Properties.Resources.Audio_Wave_48px_3;
+            this.pbWave.Location = new System.Drawing.Point(222, 78);
+            this.pbWave.Name = "pbWave";
+            this.pbWave.Size = new System.Drawing.Size(60, 46);
+            this.pbWave.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbWave.TabIndex = 55;
+            this.pbWave.TabStop = false;
+            this.pbWave.Visible = false;
             // 
-            // rdPlay
+            // pbRec
             // 
-            this.rdPlay.Name = "rdPlay";
-            this.rdPlay.Symbol = "";
-            this.rdPlay.Text = "Play";
+            this.pbRec.Image = global::PschyHealth.Properties.Resources.Record_48px_4;
+            this.pbRec.Location = new System.Drawing.Point(338, 127);
+            this.pbRec.Name = "pbRec";
+            this.pbRec.Size = new System.Drawing.Size(26, 16);
+            this.pbRec.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbRec.TabIndex = 54;
+            this.pbRec.TabStop = false;
+            this.pbRec.Visible = false;
             // 
-            // metroTextBox1
+            // lblPlayName
             // 
-            // 
-            // 
-            // 
-            this.metroTextBox1.CustomButton.Image = null;
-            this.metroTextBox1.CustomButton.Location = new System.Drawing.Point(53, 1);
-            this.metroTextBox1.CustomButton.Name = "";
-            this.metroTextBox1.CustomButton.Size = new System.Drawing.Size(21, 21);
-            this.metroTextBox1.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
-            this.metroTextBox1.CustomButton.TabIndex = 1;
-            this.metroTextBox1.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
-            this.metroTextBox1.CustomButton.UseSelectable = true;
-            this.metroTextBox1.CustomButton.Visible = false;
-            this.metroTextBox1.Lines = new string[] {
-        "metroTextBox1"};
-            this.metroTextBox1.Location = new System.Drawing.Point(207, 31);
-            this.metroTextBox1.MaxLength = 32767;
-            this.metroTextBox1.Name = "metroTextBox1";
-            this.metroTextBox1.PasswordChar = '\0';
-            this.metroTextBox1.ScrollBars = System.Windows.Forms.ScrollBars.None;
-            this.metroTextBox1.SelectedText = "";
-            this.metroTextBox1.SelectionLength = 0;
-            this.metroTextBox1.SelectionStart = 0;
-            this.metroTextBox1.Size = new System.Drawing.Size(75, 23);
-            this.metroTextBox1.TabIndex = 1;
-            this.metroTextBox1.Text = "metroTextBox1";
-            this.metroTextBox1.UseSelectable = true;
-            this.metroTextBox1.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
-            this.metroTextBox1.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            this.lblPlayName.AutoSize = true;
+            this.lblPlayName.Location = new System.Drawing.Point(212, 16);
+            this.lblPlayName.Name = "lblPlayName";
+            this.lblPlayName.Size = new System.Drawing.Size(0, 0);
+            this.lblPlayName.TabIndex = 57;
             // 
             // frmRecord
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(502, 253);
-            this.Controls.Add(this.metroTextBox1);
+            this.Controls.Add(this.lblPlayName);
+            this.Controls.Add(this.lblPlayBack);
+            this.Controls.Add(this.pbWave);
+            this.Controls.Add(this.pbRec);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.txtName);
             this.Controls.Add(this.radialMenu1);
             this.Name = "frmRecord";
             this.Text = "Record";
             this.Load += new System.EventHandler(this.frmRecord_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbWave)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbRec)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private DevComponents.DotNetBar.RadialMenu radialMenu1;
-        private DevComponents.DotNetBar.RadialMenuItem rdRecord;
-        private DevComponents.DotNetBar.RadialMenuItem rdSave;
-        private DevComponents.DotNetBar.RadialMenuItem rdPause;
+        private MetroFramework.Controls.MetroTextBox txtName;
         private DevComponents.DotNetBar.RadialMenuItem rdPlay;
-        private MetroFramework.Controls.MetroTextBox metroTextBox1;
+        private DevComponents.DotNetBar.RadialMenuItem rdPause;
+        private DevComponents.DotNetBar.RadialMenuItem rdSave;
+        private DevComponents.DotNetBar.RadialMenuItem rdRecord;
+        private DevComponents.DotNetBar.RadialMenu radialMenu1;
+        private System.Windows.Forms.Timer timer1;
+        private MetroFramework.Controls.MetroLabel lblPlayBack;
+        private System.Windows.Forms.PictureBox pbWave;
+        private System.Windows.Forms.PictureBox pbRec;
+        private MetroFramework.Controls.MetroLabel lblTime;
+        private MetroFramework.Controls.MetroLabel lblPlayName;
     }
 }
--- PschyHealth/Forms/frmRecord.cs
+++ PschyHealth/Forms/frmRecord.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,6 +14,17 @@ namespace PschyHealth.Forms
 {
     public partial class frmRecord : MetroForm
     {
+        int tempHour = 0;
+        int tempMin = 0;
+        int tempSec = 0;
+        int tempMs = 0;
+
+        [DllImport("winmm.dll")]
+        private static extern int mciSendString(string MciComando, string MciRetorno, int MciRetornoLeng, int CallBack);
+        string musica = "";
+
+        int hour, min, sec, ms = 0;
+
         Methods cMethods = new Methods();
         //Constants
         const int AW_SLIDE = 0X40000;
@@ -49,7 +61,118 @@ namespace PschyHealth.Forms
 
         private void frmRecord_Load(object sender, EventArgs e)
         {
+            txtName.Visible = true;
+        }
 
+        private void rdPause_Click(object sender, EventArgs e)
+        {
+            pbRec.Visible = false;
+            timer1.Stop();
+            mciSendString("pause Som", null, 0, 0);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblTime.Text = Convert.ToString(hour + " : " + min + " : " + sec + " : " + ms);
+            ms++;
+
+            if (ms > 10)
+            {
+                sec++;
+                ms = 0;
+            }
+            else if (sec > 60)
+            {
+                min++;
+                sec = 0;
+            }
+            else if (min > 60)
+            {
+                hour++;
+                min = 0;
+            }
+        }
+
+        private void pbExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            mciSendString("close " + musica, null, 0, 0);
+
+        }
+
+        private void pbMinimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void rdPlay_Click(object sender, EventArgs e)
+        {
+            pbWave.Visible = true;
+            lblPlayBack.Visible = true;
+            lblTime.Visible = false;
+            pbRec.Visible = false;
+
+
+
+            if (musica == "")
+            {
+                OpenFileDialog open = new OpenFileDialog();
+                open.Filter = "Wave|*.wav";
+                if (open.ShowDialog() == DialogResult.OK) { musica = open.FileName; }
+            }
+            else
+            {
+                OpenFileDialog open = new OpenFileDialog();
+                open.Filter = "Wave|*.wav";
+                if (open.ShowDialog() == DialogResult.OK) { musica = open.FileName; }
+                lblPlayName.Text = open.FileName;
+            }
+            txtName.Visible = false;
+            lblPlayName.Visible = true;
+
+            pbWave.Visible = true;
+            lblPlayBack.Visible = true;
+            mciSendString("play " + musica, null, 0, 0);
+
+
+
+
+
+
+            // mciSendString("play " + musica, null, 0, 0);
+
+        }
+
+        private void rdRecord_Click(object sender, EventArgs e)
+        {
+            txtName.Visible = true;
+            lblPlayBack.Visible = false;
+            pbWave.Visible = false;
+            pbRec.Visible = true;
+            timer1.Start();
+
+            mciSendString("open new type waveaudio alias Som", null, 0, 0);
+            mciSendString("record Som", null, 0, 0);
+        }
+
+        private void rdSave_Click(object sender, EventArgs e)
+        {
+
+
+            pbRec.Visible = false;
+            timer1.Stop();
+
+            SaveFileDialog save = new SaveFileDialog();
+
+            save.Filter = "wave|*.wav";
+
+
+            if (save.ShowDialog() == DialogResult.OK)
+            {
+
+                mciSendString("save Som " + save.FileName, null, 0, 0);
+                mciSendString("close Som", null, 0, 0);
+            }
         }
     }
 }
